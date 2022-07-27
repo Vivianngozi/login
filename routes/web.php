42c8ba59ebf6e1e40ae872f2d3a35de5ab8fdf8e
Vivianngozi <orjingozy@yahoo.com>
2022-07-27 05:47:54 +0100

    first commit

---
+++ routes/web.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use Illuminate\Support\Facades\Auth;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+
+    // if(Auth::check()){
+
+    //     return "The user is logged in";
+    // }
+
+
+    //atttempt to login
+
+    // $username = "george";
+
+    // $password = "ffghhnb3rerf";
+
+    // if(Auth::attempt(['username'=>$username, 'password'=>$password])){
+
+    //     return redirect()->intended('/admin');
+    // }
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
